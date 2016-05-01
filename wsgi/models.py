f3d4b289fc545ffb985cb51fbf376141afc61833
dredivaris <dredivaris@gmail.com>
2016-05-01 14:54:06 -0700

    added temp secret key, dummy admin models to test pymongo flask integration

--- wsgi/models.py
+++ wsgi/models.py
@@ -1,9 +1,7 @@
+
 from flask.ext.admin.contrib.pymongo import ModelView
 
 from wtforms import Form, StringField
-
-from wsgi.views import db, admin
-
 
 class UserForm(Form):
     name = StringField('Name')
@@ -13,8 +11,3 @@ class UserForm(Form):
 class UserView(ModelView):
     column_list = ('name', 'email')
     form = UserForm
-
-
-if __name__ == '__main__':
-    # 'db' is PyMongo database object
-    admin.add_view(UserView(db['users']))
