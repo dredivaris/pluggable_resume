ea1581bb9b91db2556ca6df2be55df4802dbdefe
dredivaris <dredivaris@gmail.com>
2016-05-14 13:23:09 -0700

    added login and login flash messages.  still need a registration function

--- wsgi/models.py
+++ wsgi/models.py
@@ -1,13 +1,22 @@
-
 from flask.ext.admin.contrib.pymongo import ModelView
+import flask.ext.login as login
+from wtforms import Form, StringField, PasswordField
 
-from wtforms import Form, StringField
+
+# class AuthenticatedModelView(ModelView):
+#     def is_accessible(self):
+#         return login.current_user.is_authenticated()
+
 
 class UserForm(Form):
     name = StringField('Name')
     email = StringField('Email')
+    password = PasswordField('password')
 
 
 class UserView(ModelView):
-    column_list = ('name', 'email')
+    column_list = ('name', 'email', 'password')
     form = UserForm
+
+
+
