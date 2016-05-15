cd21e655b4f3a5a77a0a9e6953e406ceee8180ab
dredivaris <dredivaris@gmail.com>
2016-05-15 09:33:11 -0700

    refactor MyModelView class out of models file

--- wsgi/admin_views.py
+++ wsgi/admin_views.py
@@ -1,9 +1,11 @@
-
-from wsgi.models import MyModelView
+from flask import url_for, request
 from flask.ext.admin import BaseView, expose
+from flask.ext.admin.contrib import mongoengine
+from flask.ext.login import current_user
 from flask_admin import helpers as admin_helpers
 from flask.ext.security import login_required
-
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 
 from wsgi.views import admin, Role, User, engine_db, security
 
@@ -14,6 +16,32 @@ class MyView(BaseView):
         return self.render('admin/woot.html')
 
 admin.add_view(MyView(name='Woot'))
+
+
+class MyModelView(mongoengine.ModelView):
+
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
+
 admin.add_view(MyModelView(Role))
 admin.add_view(MyModelView(User))
 
