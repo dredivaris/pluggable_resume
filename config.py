51a9076054ff6d205f235877c19bd01adb0a5d6c
dredivaris <dredivaris@gmail.com>
2016-05-26 16:15:41 -0700

    set up mongodo env variables on openshift vs local

--- config.py
+++ config.py
@@ -2,10 +2,18 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-MONGO_DBNAME = 'andreas_website'
 MONGODB_DB = 'andreas_website'
-MONGODB_HOST = 'localhost'
-SESSION_TYPE = 'mongodb'
+MONGO_DBNAME = 'andreas_website'
+
+if os.environ.get('OPENSHIFT_MONGODB_IDENT') is None:
+    MONGODB_HOST = os.environ.get('OPENSHIFT_MONGODB_DB_HOST')
+    MONGODB_PORT = os.environ.get('OPENSHIFT_MONGODB_DB_PORT')
+    MONGODB_USERNAME = os.environ.get('OPENSHIFT_MONGODB_DB_USERNAME')
+    MONGODB_PASSWORD = os.environ.get('OPENSHIFT_MONGODB_DB_PASSWORD')
+else:
+    MONGODB_HOST = 'localhost'
+    SESSION_TYPE = 'mongodb'
+
 SECRET_KEY = 'jkJKJGFDKJ*hG*&YD)JPSDGJLSKDJGOISUojidlsfkjasgd08adgpijG(HD('
 
 SECURITY_URL_PREFIX = "/"
