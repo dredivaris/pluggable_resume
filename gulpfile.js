47acc422dc20d551aece9586a325a90ecce49a77
dredivaris <dredivaris@gmail.com>
2016-06-24 21:50:34 -0700

    added babel transform for es2015 js!!!

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     size = require('gulp-size'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    sourcemaps = require("gulp-sourcemaps"),
+    babel = require("gulp-babel");
 
 gulp.task('transform', function () {
   return gulp.src('./wsgi/static/scripts/jsx/reddit_client.js')
     .pipe(browserify({transform: ['reactify']}))
+    .on('error', onError)
+    .pipe(babel())
     .pipe(gulp.dest('./wsgi/static/scripts/js'))
     .pipe(size());
 });
@@ -22,3 +26,8 @@ gulp.task('default', function() {
   gulp.start('transform');
   gulp.watch('./wsgi/static/scripts/jsx/reddit_client.js', ['transform']);
 });
+
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
